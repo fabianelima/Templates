d14cc38777ec6ade109f8327381a1e1c7bddbe1d
Fabiane Lima <fabianelim@gmail.com>
2018-06-06 16:32:53 -0300

    Mudanças

--- ~quiz/assets/script.coffee
+++ ~quiz/assets/script.coffee
@@ -1,5 +1,5 @@
 ###
-	       OA TEMPLATE 4.0
+	       OA TEMPLATE 4.5
 	----------------------------
 	Desenvolvido em CoffeeScript
 
@@ -26,24 +26,16 @@ $(window).on 'load', -> preload(imgs)
 # ----- Funções e dados ----- #
 $ ->
 	alt = undefined
+	pro = undefined
+	ctrl = []
 	count = 0
 	score = 0
+	error = 0
+	inOrder = 1
 	data =	[
 				{
 					titl: 'Título da questão'
-					enun: '1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-					alts: 	[
-								'Alternativa 1'
-								'Alternativa 2'
-								'Alternativa 3'
-								'Alternativa 4'
-							]
-					answ: 2
-					feed: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-				}
-				{
-					titl: 'Título da questão'
-					enun: '2. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+					enun: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
 					alts: 	[
 								'Alternativa 1'
 								'Alternativa 2'
@@ -55,27 +47,37 @@ $ ->
 				}
 				{
 					titl: 'Título da questão'
-					enun: '3. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+					enun: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
 					alts: 	[
 								'Alternativa 1'
 								'Alternativa 2'
 								'Alternativa 3'
 								'Alternativa 4'
 							]
-					answ: 3
+					answ: 1
+					feed: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+				}
+				{
+					titl: 'Título da questão'
+					enun: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+					alts: 	[
+								'Alternativa 1'
+								'Alternativa 2'
+								'Alternativa 3'
+								'Alternativa 4'
+							]
+					answ: 2
 					feed: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 				}
 			]
 	func =
 		help: ->
-			$('.content').fadeOut()
 			$('.dimmer').fadeIn()
 			$('.modal').html('<h1>Ajuda</h1><p></p><button class="dismiss">Fechar</button>')
 
 		info: -> $('.modal').html('<h1>Referência</h1><p></p><button class="end">Voltar</button>')
 
 		end: ->
-			$('.content').fadeOut()
 			$('.dimmer').fadeIn()
 
 			if score > 1 then $('.modal').html('<h1>Você acertou ' + score + ' questões!</h1><p></p><button class="info">Referência</button>&nbsp;&nbsp;<button class="again">Ver novamente</button>')
@@ -84,26 +86,34 @@ $ ->
 
 			$('.modal p').html('oioioi')
 
-		dismiss: ->
-			$('.content').fadeIn()
-			$('.dimmer').fadeOut()
+		dismiss: -> $('.dimmer').fadeOut()
+
+		rNumber: ->
+			randy = Math.floor(Math.random() * data.length)
+
+			if ctrl.length < data.length
+				if ctrl.indexOf(randy) is -1
+					ctrl.push randy
+					$('.quiz').append('
+						<section id="' + randy + '" class="q">
+							<div class="enun">
+								<h1>' + (ctrl.indexOf(randy) + 1) + '. ' + data[randy].titl + '</h1>
+								<p>' + data[randy].enun + '</p>
+							</div>
+							<div class="alts"><ul></ul></div>
+							<button class="verify">Conferir</button>
+						</section>
+					')
+					pro = true
+				else pro = false
+
+				func.putAlts(randy)
+				func.rNumber()
 
 		start: ->
-			j = 0
-
-			for i in data
-				$('.quiz').append('
-					<section>
-						<div class="enun"><h3>' + data[j].titl + '</h3>
-						<p>' + data[j].enun + '</p></div>
-						<div class="alts"><ul></ul></div>
-						<button class="verify">Conferir</button>
-					</section>
-				')
-				j++
-
-			func.putAlts()
+			func.rNumber()
 			func.dismiss()
+			$('.content').fadeIn()
 
 		selectAlt: ($el) ->
 			alt = $el.index()
@@ -112,15 +122,16 @@ $ ->
 			$el.css { background: '#006c7f', color: 'white' }
 
 		verify: ->
-			$('.content').fadeOut()
-			$('.dimmer').fadeIn()
-			$('.modal').html('<h1></h1><p>' + data[count].feed + '</p><button class="nxt">Próxima</button>')
+			$('.dimmer').delay(500).fadeIn()
+			$('.modal').html('<h1></h1><p>' + data[ctrl[count]].feed + '</p><button class="nxt">Prosseguir</button>')
 
-			if alt is data[count].answ
+			if alt is data[ctrl[count]].answ
 				score++
 				$('.modal h1').html('Resposta certa!')
 
-			else $('.modal h1').html('Resposta errada!')
+			else
+				error++
+				$('.modal h1').html('Resposta errada!')
 
 		nxt: ->
 			count++
@@ -137,13 +148,19 @@ $ ->
 					func.end()
 				, 400
 
-		putAlts: ->
-			l = 0
-			for k in data[count].alts
-				$('.quiz section:nth-child(' + (count + 1) + ') .alts ul').append('
-					<li>' + data[count].alts[l] + '</li>
-				')
-				l++
+		putAlts: (randy) ->
+			testPromise = new Promise (resolve, reject) ->
+				if pro is true then resolve()
+				else reject()
+
+			.then (fromResolve) ->
+				for i, j in data[randy].alts
+					$('.quiz section:nth-child(' + inOrder + ') .alts ul').append('<li>' + i + '</li>')
+
+					if j is data[randy].alts.length - 1 then inOrder++
+					console.log data[randy].answ
+
+			.catch (fromReject) -> return
 
 
 # ----- Eventos ----- #
--- ~quiz/assets/script.js
+++ ~quiz/assets/script.js
@@ -1,6 +1,6 @@
 
 /*
-	       OA TEMPLATE 4.0
+	       OA TEMPLATE 4.5
 	----------------------------
 	Desenvolvido em CoffeeScript
 
@@ -36,34 +36,37 @@
   });
 
   $(function() {
-    var alt, count, data, func, score;
+    var alt, count, ctrl, data, error, func, inOrder, pro, score;
     alt = void 0;
+    pro = void 0;
+    ctrl = [];
     count = 0;
     score = 0;
+    error = 0;
+    inOrder = 1;
     data = [
       {
         titl: 'Título da questão',
-        enun: '1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        alts: ['Alternativa 1', 'Alternativa 2', 'Alternativa 3', 'Alternativa 4'],
-        answ: 2,
-        feed: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-      }, {
-        titl: 'Título da questão',
-        enun: '2. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        enun: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         alts: ['Alternativa 1', 'Alternativa 2', 'Alternativa 3', 'Alternativa 4'],
         answ: 0,
         feed: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
       }, {
         titl: 'Título da questão',
-        enun: '3. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        enun: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         alts: ['Alternativa 1', 'Alternativa 2', 'Alternativa 3', 'Alternativa 4'],
-        answ: 3,
+        answ: 1,
+        feed: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      }, {
+        titl: 'Título da questão',
+        enun: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        alts: ['Alternativa 1', 'Alternativa 2', 'Alternativa 3', 'Alternativa 4'],
+        answ: 2,
         feed: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
       }
     ];
     func = {
       help: function() {
-        $('.content').fadeOut();
         $('.dimmer').fadeIn();
         return $('.modal').html('<h1>Ajuda</h1><p></p><button class="dismiss">Fechar</button>');
       },
@@ -71,7 +74,6 @@
         return $('.modal').html('<h1>Referência</h1><p></p><button class="end">Voltar</button>');
       },
       end: function() {
-        $('.content').fadeOut();
         $('.dimmer').fadeIn();
         if (score > 1) {
           $('.modal').html('<h1>Você acertou ' + score + ' questões!</h1><p></p><button class="info">Referência</button>&nbsp;&nbsp;<button class="again">Ver novamente</button>');
@@ -83,19 +85,27 @@
         return $('.modal p').html('oioioi');
       },
       dismiss: function() {
-        $('.content').fadeIn();
         return $('.dimmer').fadeOut();
       },
-      start: function() {
-        var i, j, len, m;
-        j = 0;
-        for (m = 0, len = data.length; m < len; m++) {
-          i = data[m];
-          $('.quiz').append('<section> <div class="enun"><h3>' + data[j].titl + '</h3> <p>' + data[j].enun + '</p></div> <div class="alts"><ul></ul></div> <button class="verify">Conferir</button> </section>');
-          j++;
+      rNumber: function() {
+        var randy;
+        randy = Math.floor(Math.random() * data.length);
+        if (ctrl.length < data.length) {
+          if (ctrl.indexOf(randy) === -1) {
+            ctrl.push(randy);
+            $('.quiz').append('<section id="' + randy + '" class="q"> <div class="enun"> <h1>' + (ctrl.indexOf(randy) + 1) + '. ' + data[randy].titl + '</h1> <p>' + data[randy].enun + '</p> </div> <div class="alts"><ul></ul></div> <button class="verify">Conferir</button> </section>');
+            pro = true;
+          } else {
+            pro = false;
+          }
+          func.putAlts(randy);
+          return func.rNumber();
         }
-        func.putAlts();
-        return func.dismiss();
+      },
+      start: function() {
+        func.rNumber();
+        func.dismiss();
+        return $('.content').fadeIn();
       },
       selectAlt: function($el) {
         alt = $el.index();
@@ -113,13 +123,13 @@
         });
       },
       verify: function() {
-        $('.content').fadeOut();
-        $('.dimmer').fadeIn();
-        $('.modal').html('<h1></h1><p>' + data[count].feed + '</p><button class="nxt">Próxima</button>');
-        if (alt === data[count].answ) {
+        $('.dimmer').delay(500).fadeIn();
+        $('.modal').html('<h1></h1><p>' + data[ctrl[count]].feed + '</p><button class="nxt">Prosseguir</button>');
+        if (alt === data[ctrl[count]].answ) {
           score++;
           return $('.modal h1').html('Resposta certa!');
         } else {
+          error++;
           return $('.modal h1').html('Resposta errada!');
         }
       },
@@ -140,17 +150,28 @@
           }, 400);
         }
       },
-      putAlts: function() {
-        var k, l, len, m, ref, results;
-        l = 0;
-        ref = data[count].alts;
-        results = [];
-        for (m = 0, len = ref.length; m < len; m++) {
-          k = ref[m];
-          $('.quiz section:nth-child(' + (count + 1) + ') .alts ul').append('<li>' + data[count].alts[l] + '</li>');
-          results.push(l++);
-        }
-        return results;
+      putAlts: function(randy) {
+        var testPromise;
+        return testPromise = new Promise(function(resolve, reject) {
+          if (pro === true) {
+            return resolve();
+          } else {
+            return reject();
+          }
+        }).then(function(fromResolve) {
+          var i, j, k, len, ref, results;
+          ref = data[randy].alts;
+          results = [];
+          for (j = k = 0, len = ref.length; k < len; j = ++k) {
+            i = ref[j];
+            $('.quiz section:nth-child(' + inOrder + ') .alts ul').append('<li>' + i + '</li>');
+            if (j === data[randy].alts.length - 1) {
+              inOrder++;
+            }
+            results.push(console.log(data[randy].answ));
+          }
+          return results;
+        })["catch"](function(fromReject) {});
       }
     };
     $(document).on('click', '.start', function() {
